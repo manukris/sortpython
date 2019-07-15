f9bc12514cd13fd4a00619caf1482b159ad0140d
manukris <manukrishnatesro@gmail.com>
2019-07-15 09:18:53 +0530

    Selection Sort analysis

--- selectionsort.py
+++ selectionsort.py
@@ -1,1 +1,24 @@
-print("hello")
+array = [100, 197, 83, 65, 57, 44, 33, 25]
+
+n = len(array)
+
+for x in array:
+    indexx = array.index(x)
+    for y in array[indexx + 1:]:
+
+        if x > y:
+            indexy = array.index(y)
+            t = array[indexx]
+            array[indexx] = array[indexy]
+            array[indexy] = t
+
+print(array)
+
+
+
+
+
+
+
+
+
